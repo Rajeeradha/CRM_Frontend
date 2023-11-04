4039c99a80eb0ac4db91e1512a93af4187fa192b
Rajeeradha <127470577+Rajeeradha@users.noreply.github.com>
2023-11-04 17:15:29 +1100

    Update UpdateServiceRequest.js

--- src/Components/ServiceRequest/UpdateServiceRequest.js
+++ src/Components/ServiceRequest/UpdateServiceRequest.js
@@ -33,7 +33,7 @@ const UpdateServiceRequest = () => {
     }).catch(error => {
         console.log('Error: ', error);
     })
-}, [params.id])
+}, [userID,token])
 
 const handleChange = (value) => {
     return setServiceRequestDetails((serviceRequest) => {
